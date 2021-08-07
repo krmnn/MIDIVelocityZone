3e8c85d05abc5b50fa2e16708607d578dea3e339
Thomas Karmann <thomas@krmnn.de>
2021-08-07 13:17:28 +0200

    Prepare ci for FRUT cmake module

--- Source/OJDParameters.h
+++ Source/OJDParameters.h
@@ -1,23 +1,23 @@
 /*
- 
- This file is part of the Schrammel OJD audio plugin.
- Copyright (C) 2020  Janos Buttgereit
- 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software Foundation,
- Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
- 
- */
+
+   This file is part of the Schrammel OJD audio plugin.
+   Copyright (C) 2020  Janos Buttgereit
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+*/
 
 #pragma once
 
@@ -32,31 +32,19 @@ struct OJDParameters
 {
     struct Sliders
     {
-        //        /** All three rotary sliders display a typical guitar equipment value range from 0 to 10 */
-        //        static const juce::NormalisableRange<float> displayRange;
-        //
-        //        /** Takes the raw 0 to 10 parameter value and returns a 0 to 1 value */
-        //        static float normaliseRawValue (const std::atomic<float>& rawValue);
-        //
-        //        struct Drive
-        //        {
-        //            static const juce::String                   id;
-        //        };
-        //
-        //        struct Tone
-        //        {
-        //            static const juce::String                   id;
-        //        };
-        //
-        //        struct Volume
-        //        {
-        //            static const juce::String                   id;
-        //
-        //            /** Takes the raw 0 to 10 parameter value and returns a -60dB to -20dB value */
-        //            static float dBValueFromRawValue (const std::atomic<float>& rawValue);
-        //        };
+        /** All three rotary sliders display a typical guitar equipment value range from 0 to 10 */
+        static const juce::NormalisableRange<float> displayRange;
+
+        /** Takes the raw 0 to 10 parameter value and returns a 0 to 1 value */
+        static float normaliseRawValue (const std::atomic<float>& rawValue);
+
+        struct Kick
+        {
+            static const juce::String                   id;
+        };
+
     };
-    
+
     struct Switches
     {
         //        class HpLp
@@ -75,25 +63,25 @@ struct OJDParameters
         //
         class Bypass
         {
-        public:
-            static const juce::String id;
-            
-            /** Returns true if the bypass is active, e.g. if audio should be bypassed */
-            static bool isActive (const std::atomic<float>& rawValue);
-            
-        private:
-            friend OJDParameters;
-            static juce::String stringFromBoolConversion (bool isBypassed, int maximumStringLength);
-            static bool         boolFromStringConversion (const juce::String& text);
+            public:
+                static const juce::String id;
+
+                /** Returns true if the bypass is active, e.g. if audio should be bypassed */
+                static bool isActive (const std::atomic<float>& rawValue);
+
+            private:
+                friend OJDParameters;
+                static juce::String stringFromBoolConversion (bool isBypassed, int maximumStringLength);
+                static bool         boolFromStringConversion (const juce::String& text);
         };
     };
-    
+
     /** Used to report the Bypass parameter to PluginAudioProcessorBase */
     using Bypass = Switches::Bypass;
-    
+
     /** Supplies the parameter layout used to create the AudioProcessorValueTreeState */
     static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-    
+
     /**
      * The preset manager will listen to all parameters in this array. Changing those will make the
      * preset state turn "dirty"
--- Source/ProcessorEditor.cpp
+++ Source/ProcessorEditor.cpp
@@ -1,23 +1,23 @@
 /*
- 
- This file is part of the Schrammel OJD audio plugin.
- Copyright (C) 2020  Janos Buttgereit
- 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software Foundation,
- Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
- 
- */
+
+   This file is part of the Schrammel OJD audio plugin.
+   Copyright (C) 2020  Janos Buttgereit
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+*/
 
 #include "PluginProcessor.h"
 #include "ProcessorEditor.h"
@@ -26,153 +26,36 @@ juce::Rectangle<int> scaledKeepingCentre (const juce::Rectangle<int>& src, float
 {
     auto centre = src.getCentre().toFloat();
     auto scaled = src.toFloat() * scale;
-    
+
     scaled.setCentre (centre);
-    
+
     return scaled.toNearestInt();
 }
 
-//OJDPedalComponent::OJDPedalComponent (OJDAudioProcessor &proc, ProcessorEditor& e)
-//  : editor       (e),
-//    housing      (BinaryData::pedalHousing_svg, BinaryData::pedalHousing_svgSize),
-//    driveSlider  (proc.parameters, OJDParameters::Sliders::Drive::id),
-//    toneSlider   (proc.parameters, OJDParameters::Sliders::Tone::id),
-//    volumeSlider (proc.parameters, OJDParameters::Sliders::Volume::id),
-//    bypassLED    (proc.parameters, OJDParameters::Switches::Bypass::id, BinaryData::ledOn_svg,            BinaryData::ledOn_svgSize,            BinaryData::ledOff_svg, BinaryData::ledOff_svgSize, "LED"),
-//    bypassSwitch (proc.parameters, OJDParameters::Switches::Bypass::id, BinaryData::bypassBackground_svg, BinaryData::bypassBackground_svgSize, juce::Rectangle<float> (0.3f, 0.1f, 0.9f, 0.9f)),
-//    hpLpSwitch   (proc.parameters, OJDParameters::Switches::HpLp::id,   BinaryData::hpLpBackground_svg,   BinaryData::hpLpBackground_svgSize,   juce::Rectangle<float> (0.05f, 0.05f, 0.9f, 0.9f))
-//{
-//    housingShadow.setImage (juce::ImageFileFormat::loadFrom (BinaryData::pedalHousingShadow_png, BinaryData::pedalHousingShadow_pngSize));
-//    shadowOverlay.setImage (juce::ImageFileFormat::loadFrom (BinaryData::shadowOverlay_png, BinaryData::shadowOverlay_pngSize));
-//
-//    addAndMakeVisible (housingShadow);
-//    addAndMakeVisible (housing);
-//
-//    addAndMakeVisible (shadowOverlay);
-//
-//    addSliderAndSetStyle (volumeSlider);
-//    addSliderAndSetStyle (driveSlider);
-//    addSliderAndSetStyle (toneSlider);
-//
-//    addBypassElementsAndSetStyle();
-//
-//    addHpLpSwitchAndSetStyle();
-//}
-//
-//void OJDPedalComponent::resized()
-//{
-//    auto bounds = getLocalBounds();
-//
-//    auto hb = scaledKeepingCentre (bounds, 0.94f);
-//    housing.setBounds (hb);
-//    housingShadow.setBounds (hb);
-//    shadowOverlay.setBounds (hb);
-//
-//    layouts.recalculate (bounds);
-//
-//    // set sizes
-//    hpLpSwitch  .setSize (layouts.hpLpWidth,   layouts.hpLpHeight);
-//    bypassSwitch.setSize (layouts.bypassWidth, layouts.bypassHeight);
-//    bypassLED   .setSize (layouts.ledWidth,    layouts.ledHeight);
-//    volumeSlider.setSize (layouts.volumeWidth, layouts.volumeHeight);
-//    driveSlider .setSize (layouts.driveWidth,  layouts.driveHeight);
-//    toneSlider  .setSize (layouts.toneWidth,   layouts.toneHeight);
-//
-//    // setCentres
-//    hpLpSwitch  .setCentrePosition (layouts.hpLpCentre);
-//    bypassSwitch.setCentrePosition (layouts.bypassCentre);
-//    bypassLED   .setCentrePosition (layouts.ledCentre);
-//    volumeSlider.setCentrePosition (layouts.volumeCentre);
-//    driveSlider .setCentrePosition (layouts.driveCentre);
-//    toneSlider  .setCentrePosition (layouts.toneCentre);
-//}
-//
-//void OJDPedalComponent::addSliderAndSetStyle (AttachedSlider& slider)
-//{
-//    slider.setSliderStyle  (juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
-//    slider.setTextBoxStyle (juce::Slider::NoTextBox, true, 0, 0);
-//    slider.setColour       (juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::black);
-//    slider.setPopupDisplayEnabled (true, true, this);
-//
-//    addAndMakeVisible (slider);
-//    editor.registerHighlightableWidget (slider);
-//}
-//
-//void OJDPedalComponent::addBypassElementsAndSetStyle()
-//{
-//    bypassSwitch.setClickingTogglesState (true);
-//
-//    bypassLED.setClickingTogglesState (false);
-//
-//    addAndMakeVisible (bypassSwitch);
-//    addAndMakeVisible (bypassLED);
-//
-//    editor.registerHighlightableWidget (bypassSwitch);
-//}
-//
-//void OJDPedalComponent::addHpLpSwitchAndSetStyle()
-//{
-//    hpLpSwitch.setClickingTogglesState (true);
-//
-//    addAndMakeVisible (hpLpSwitch);
-//
-//    editor.registerHighlightableWidget (hpLpSwitch);
-//}
-//
-//void OJDPedalComponent::SubcomponentLayouts::recalculate (juce::Rectangle<int> bounds)
-//{
-//    hpLpCentre.x   = bounds.proportionOfWidth (0.5f);
-//    bypassCentre.x = bounds.proportionOfWidth (0.57f);
-//    ledCentre.x    = bounds.proportionOfWidth (0.5f);
-//    volumeCentre.x = bounds.proportionOfWidth (0.34f);
-//    driveCentre.x  = bounds.proportionOfWidth (0.68f);
-//    toneCentre.x   = bounds.proportionOfWidth (0.507f);
-//
-//    hpLpCentre.y   = bounds.proportionOfHeight (0.31f);
-//    bypassCentre.y = bounds.proportionOfHeight (0.833f);
-//    ledCentre.y    = bounds.proportionOfHeight (0.11f);
-//    volumeCentre.y = bounds.proportionOfHeight (0.225f);
-//    driveCentre.y  = bounds.proportionOfHeight (0.225f);
-//    toneCentre.y   = bounds.proportionOfHeight (0.433f);
-//
-//    hpLpWidth   = bounds.proportionOfWidth (0.13f);
-//    bypassWidth = bounds.proportionOfWidth (0.13f);
-//    ledWidth    = bounds.proportionOfWidth (0.13f);
-//    volumeWidth = bounds.proportionOfWidth (0.27f);
-//    driveWidth  = bounds.proportionOfWidth (0.27f);
-//    toneWidth   = bounds.proportionOfWidth (0.27f);
-//
-//    hpLpHeight   = bounds.proportionOfHeight (0.0362f);
-//    bypassHeight = bounds.proportionOfHeight (0.036f);
-//    ledHeight    = bounds.proportionOfHeight (0.155f);
-//    volumeHeight = bounds.proportionOfHeight (0.14f);
-//    driveHeight  = bounds.proportionOfHeight (0.14f);
-//    toneHeight   = bounds.proportionOfHeight (0.14f);
-//}
 
 //==============================================================================
 ProcessorEditor::ProcessorEditor (VelocityZoneAudioProcessor& proc)
-:  jb::PluginEditorBase<contentMinWidth, overallMinHeight> (proc, IsResizable::Yes, UseConstrainer::Yes),
-//background       (BinaryData::background_svg, BinaryData::background_svgSize),
-//pedal            (proc, *this),
-isInMessageState (false),
-messageOkButton ("OK"),
-messageLearnMoreButton ("Learn more")
-//knobDrawable     (juce::Drawable::createFromImageData (juce::BinaryData::knob_svg, BinaryData::knob_svgSize)),
-//ojdLookAndFeel   (*knobDrawable)
+    :  jb::PluginEditorBase<contentMinWidth, overallMinHeight> (proc, IsResizable::Yes, UseConstrainer::Yes),
+    //background       (BinaryData::background_svg, BinaryData::background_svgSize),
+    //pedal            (proc, *this),
+    isInMessageState (false),
+    messageOkButton ("OK"),
+    messageLearnMoreButton ("Learn more")
+       //knobDrawable     (juce::Drawable::createFromImageData (juce::BinaryData::knob_svg, BinaryData::knob_svgSize)),
+       //ojdLookAndFeel   (*knobDrawable)
 {
     //setLookAndFeel (&ojdLookAndFeel);
-    
+
     //addAndMakeVisible (background);
     //addAndMakeVisible (pedal);
     //addAndMakeVisible (bypassSwitch);
-    
-    
+
+
     addPresetManager (proc);
     setupAndAddMessageOfTheDayComponents();
-    
+
     restoreSizeFromState();
-    
+
     //checkMessageOfTheDay (proc);
 }
 
@@ -184,29 +67,29 @@ ProcessorEditor::~ProcessorEditor()
 void ProcessorEditor::constrainedResized()
 {
     presetManagerComponent->setBounds (getLocalBounds().removeFromTop (presetManagerComponentHeight));
-    
+
     auto contentBounds = getLocalBounds().withTrimmedTop (presetManagerComponentHeight);
     //background.setBounds (contentBounds);
-    
+
     auto pedalBounds = scaledKeepingCentre (contentBounds, 0.85f);
-    
+
     if (isInMessageState)
     {
         pedalBounds.translate (proportionOfWidth (0.8f), 0);
-        
+
         auto messageBounds = contentBounds.reduced (proportionOfHeight (0.05f));
-        
+
         messageEditor.applyFontToAllText(getHeight() * 0.025f);
         messageEditor.setBounds (messageBounds.removeFromTop (proportionOfHeight (0.8f)));
-        
+
         messageBounds.removeFromTop (proportionOfHeight (0.01f));
-        
+
         auto okBounds = messageBounds.removeFromLeft (messageBounds.getWidth() / 2);
         auto learnMoreBounds = messageBounds;
         messageOkButton.setBounds (okBounds.reduced (proportionOfHeight (0.01f)));
         messageLearnMoreButton.setBounds (learnMoreBounds.reduced (proportionOfHeight (0.01f)));
     }
-    
+
     //pedal.setBounds (pedalBounds);
 }
 
@@ -214,7 +97,7 @@ void ProcessorEditor::checkBounds (juce::Rectangle<int>& bounds, const juce::Rec
 {
     auto contentHeight = juce::jmax (contentMinHeight, bounds.getHeight() - presetManagerComponentHeight);
     auto contentWidth  = juce::jmax (contentMinWidth, juce::roundToInt (contentHeight * contentAspectRatio));
-    
+
     bounds.setSize (contentWidth, contentHeight + presetManagerComponentHeight);
 }
 
@@ -234,26 +117,26 @@ void ProcessorEditor::setupAndAddMessageOfTheDayComponents()
     addChildComponent(messageEditor);
     addChildComponent (messageOkButton);
     addChildComponent (messageLearnMoreButton);
-    
+
     messageEditor.setReadOnly (true);
     messageEditor.setColour (juce::TextEditor::ColourIds::backgroundColourId, juce::Colours::transparentBlack);
     messageEditor.setColour (juce::TextEditor::ColourIds::outlineColourId, juce::Colours::transparentBlack);
     messageEditor.setColour (juce::TextEditor::ColourIds::textColourId, juce::Colours::white);
-    
+
     messageEditor.setJustification (juce::Justification::centred);
     messageEditor.setMultiLine (true);
-    
+
     messageOkButton.setColour (juce::TextButton::ColourIds::buttonColourId, juce::Colours::transparentBlack);
     messageLearnMoreButton.setColour (juce::TextButton::ColourIds::buttonColourId, juce::Colours::transparentBlack);
-    
+
     messageOkButton.onClick = [this]()
     {
         isInMessageState = false;
-        
+
         messageEditor.setVisible (false);
         messageOkButton.setVisible (false);
         messageLearnMoreButton.setVisible (false);
-        
+
         constrainedResized();
         repaint();
     };
@@ -262,18 +145,18 @@ void ProcessorEditor::setupAndAddMessageOfTheDayComponents()
 void ProcessorEditor::setMessage (const juce::String& text, const juce::URL url)
 {
     messageEditor.setText (text);
-    
+
     messageLearnMoreButton.onClick = [url] ()
     {
         url.launchInDefaultBrowser();
     };
-    
+
     messageEditor.setVisible (true);
     messageOkButton.setVisible (true);
     messageLearnMoreButton.setVisible (true);
-    
+
     isInMessageState = true;
-    
+
     constrainedResized();
     repaint();
 }
